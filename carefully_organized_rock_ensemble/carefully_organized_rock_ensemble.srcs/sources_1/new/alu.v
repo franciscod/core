6db249c7be0185fcdc288ba2f135d1c44813a414
fd <fd@local>
2023-06-06 20:41:06 -0300

    add empty vivado project

---
+++ carefully_organized_rock_ensemble/carefully_organized_rock_ensemble.srcs/sources_1/new/alu.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/06/2023 08:39:44 PM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+    input a,
+    input b,
+    input op,
+    output r
+    );
+endmodule
