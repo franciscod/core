7447403df2137f3b8d8c3f7b0dc3a5374b6116fe
Ignacio Losiggio <iglosiggio@gmail.com>
2023-06-14 22:10:20 -0300

    Agrego add/sub/cmp

--- carefully_organized_rock_ensemble/carefully_organized_rock_ensemble.srcs/sources_1/new/alu.v
+++ carefully_organized_rock_ensemble/carefully_organized_rock_ensemble.srcs/sources_1/new/alu.v
@@ -30,15 +30,28 @@ module alu(
     output [7:0] r,           
     output [3:0] flags         
     );
+
+    wire [8:0] a_plus_b = a + b;
+    wire [8:0] a_sub_b = a - b;
  
     assign r = op == 'b000 ? a&b
              : op == 'b001 ? a|b
              : op == 'b010 ? a^b
+             : op == 'b011 ? a_plus_b[7:0]
+             : op == 'b100 ? a_sub_b[7:0]
+             : op == 'b101 ? a_sub_b[7:0]
              : 'bz;
 
-    assign flags[0] = r == 'b0; // Z
-    assign flags[1] = 0; // V
-    assign flags[2] = r[7]; // N
-    assign flags[3] = 0; // C
-    
+    wire c = op == 'b011 ? a_plus_b[8]
+           : op == 'b100 ? a_sub_b[8]
+           : op == 'b101 ? a_sub_b[8]
+           : 0;
+    wire n = r[7];
+    wire v = op == 'b011 ? a[7] == b[7] && a[7] != r[7]
+           : op == 'b100 ? a[7] != b[7] && a[7] != r[7]
+           : op == 'b101 ? a[7] != b[7] && a[7] != r[7]
+           : 0;
+    wire z = r == 0;
+    assign flags = ({ c, n, v, z });
+
 endmodule
