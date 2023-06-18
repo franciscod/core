daaf8e760bc2e80a08db44170aa4ae04686f4741
Ignacio Losiggio <iglosiggio@gmail.com>
2023-06-18 17:44:37 -0300

    Banco de registros

--- carefully_organized_rock_ensemble/carefully_organized_rock_ensemble.srcs/sim_1/new/registers_test.v
+++ carefully_organized_rock_ensemble/carefully_organized_rock_ensemble.srcs/sim_1/new/registers_test.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 06/06/2023 08:45:11 PM
+// Create Date: 18.06.2023 14:41:10
 // Design Name: 
-// Module Name: alu_test
+// Module Name: registers_test
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,31 +19,32 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module alu_test();
+
+module registers_test();
     reg clk, reset, sl;
     reg [2:0] op, simm;
     reg [3:0] exp_flags;
     reg [7:0] a, b, exp_r;
-
+    
     wire [7:0] r;
     wire [3:0] flags;
-
+    
     reg [34:0] vectornum, errors;
     reg [34:0] testvector [0:1000];
     alu dut(
-        .op(op),
-        .a(a),
-        .b(b),
+        .op(op), 
+        .a(a), 
+        .b(b), 
         .shift_logical(sl),
         .shift_imm(simm),
         .r(r),
         .flags(flags)
     );
-
-    always 
-        #5 clk = ~clk;
-
-    initial begin
+    
+    always #5 clk = ~clk;
+    
+    initial
+    begin
         clk = 0;
         $readmemb("testdata_alu.mem", testvector);
         vectornum = 0;
@@ -52,22 +53,24 @@ module alu_test();
         #10; 
         reset = 0;
     end
-
+    
     always @(posedge clk) begin
         $display ( " %d vectornum loading " , vectornum) ;
         {op, a, b, sl, simm, exp_r, exp_flags} = testvector [vectornum];
     end
-
-    always @(negedge clk) begin
-        if (~reset) begin
-            if (({r, flags} !== {exp_r, exp_flags})) begin
+    always @(negedge clk)
+    begin        if (~reset)
+        begin
+            if (({r, flags} !== {exp_r, exp_flags}))
+            begin
                 $display (" Error:  inputs = %b %b %b %b %b", op, a, b, sl, simm);
                 $display ("        outputs = %b %b (expected = %b %b )", r, flags, exp_r, exp_flags) ;
                 errors = errors + 1;
             end
             vectornum = vectornum + 1;
-
-            if ( testvector [vectornum] === 'bX) begin
+            
+            if ( testvector [vectornum] === 'bX)
+            begin
                 $display ( " %d test completed with %d errors " , vectornum, errors ) ;
                 $finish ;
             end
---
+++ carefully_organized_rock_ensemble/carefully_organized_rock_ensemble.srcs/sources_1/new/registers.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.06.2023 14:40:37
+// Design Name: 
+// Module Name: registers
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module registers(
+    input        clk,
+    input        rst,
+    input  [3:0] sel_read_a,
+    input  [3:0] sel_read_b,
+    input        en_write_reg,
+    input  [3:0] sel_write_reg,
+    input  [7:0] data_write_reg,
+    input        en_write_flags,
+    input  [3:0] data_write_flags,
+    input        en_write_ip,
+    input  [7:0] data_write_ip,
+
+    output [7:0] r_read_a,
+    output [7:0] r_read_b,
+    output [7:0] r_flags,
+    output [7:0] r_ip,
+    output [7:0] r_cs,
+    output [7:0] r_ds
+);
+    reg [7:0] bank [0:15];
+
+    assign r_read_a = sel_read_a ==  0 ? 'b0000_0000
+                    : sel_read_a == 15 ? 'b1111_1111
+                    : bank[sel_read_a];
+    assign r_read_b = sel_read_b ==  0 ? 'b0000_0000
+                    : sel_read_b == 15 ? 'b1111_1111
+                    : bank[sel_read_b];
+    assign r_flags = bank[ 8];
+    assign r_ip    = bank[ 9];
+    assign r_cs    = bank[12];
+    assign r_ds    = bank[13];
+
+    always @(negedge clk) begin
+        // If no reset was requested just do normal operation
+        if (rst == 0) begin
+            // Write register as requested
+            if (en_write_reg) begin
+                bank[sel_write_reg] <= data_write_reg;
+            end
+            // Update flags unless a register was requested
+            if (en_write_flags && !(en_write_reg && sel_write_reg == 8)) begin
+                bank[8] <= data_write_flags;
+            end
+            // Update ip unless a register write was requested
+            if (en_write_ip && !(en_write_reg && sel_write_reg == 9)) begin
+                bank[9] <= bank[9];
+            end
+        end
+        // Otherwise put the register bank in a well-known state
+        else begin
+            bank[ 0] <= 0;
+            bank[ 1] <= 0;
+            bank[ 2] <= 0;
+            bank[ 3] <= 0;
+            bank[ 4] <= 0;
+            bank[ 5] <= 0;
+            bank[ 6] <= 0;
+            bank[ 7] <= 0;
+            bank[ 8] <= 0;
+            bank[ 9] <= 0;
+            bank[10] <= 0;
+            bank[11] <= 0;
+            bank[12] <= 0;
+            bank[13] <= 0;
+            bank[14] <= 0;
+            bank[15] <= 0;
+        end
+    end
+endmodule
