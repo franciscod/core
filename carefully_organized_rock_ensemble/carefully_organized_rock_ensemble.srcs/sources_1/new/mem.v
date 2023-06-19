41a4730ae0e73ad4effb9c715a24b9da2d1af6e3
fd <fd@local>
2023-06-19 18:05:38 -0300

    add mem source

---
+++ carefully_organized_rock_ensemble/carefully_organized_rock_ensemble.srcs/sources_1/new/mem.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/19/2023 06:05:10 PM
+// Design Name: 
+// Module Name: mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mem(
+
+    );
+endmodule
