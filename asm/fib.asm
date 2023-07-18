4ea430426f7f4f6d04ee024fb67b41a6c224fdb5
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2023-07-18 13:59:52 -0300

    Output de fib.asm vía leds

--- asm/fib.asm
+++ asm/fib.asm
@@ -2,7 +2,7 @@ movi r1, 0
 MoVi r2, 1
 
 loop:
-    storer [r0], r1
-    add      r1, r2
-    xchg     r1, r2 
-    movi     r9, loop # pc
+    storei 0x3FF, r1
+    add    r1, r2
+    xchg   r1, r2
+    movi   r9, loop # pc
